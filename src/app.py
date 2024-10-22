6f68198c4fb7cc003118de805eb5f3f778a3b1aa
Victor Botez <victor.botez88@e-uvt.ro>
2024-10-22 18:43:54 +0000

    upload

--- src/app.py
+++ src/app.py
@@ -1,2 +1,11 @@
-print("Hello, this is your app running inside a Docker container!")
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, Docker!"
+
+if __name__ == "__main__":
+    # This is where you specify the host and port
+    app.run(port=8080)
